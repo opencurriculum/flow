6486a5ad65d3600e800e153225a91f5c7f61d2cb
Varun Arora <varunarora.mct@gmail.com>
2022-08-04 16:45:48 -0700

    Initial layout shouldn't have anything that the user can't easy modify

--- pages/admin/app/[appid]/flow/[flowid]/step/[stepid].tsx
+++ pages/admin/app/[appid]/flow/[flowid]/step/[stepid].tsx
@@ -32,8 +32,8 @@ const StepWrapper: NextPage = ({ app, userID }: AppProps) => {
 
 
 const initialLayout = [
-  { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
-  { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+  { i: "a", x: 0, y: 0, w: 1, h: 2 },
+  { i: "b", x: 1, y: 0, w: 3, h: 2 },
   { i: "c", x: 4, y: 0, w: 1, h: 2 }
 ];
 
