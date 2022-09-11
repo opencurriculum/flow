35a3548e82dafda5ef6012c888eee4fe66b43361
Varun Arora <varunarora.mct@gmail.com>
2022-09-11 11:35:40 -0700

    Fix wrong API key reference

--- pages/api/lti/submit-score.ts
+++ pages/api/lti/submit-score.ts
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
     const {ltik, lineItemId, userID, score} = req.body
-    
-    const authorizationHeader = `LTIK-AUTH-V1 Token=${ltik}, Additional=Bearer ${process.env.API_KEY}`
+
+    const authorizationHeader = `LTIK-AUTH-V1 Token=${ltik}, Additional=Bearer ${process.env.LTIAAS_PRIVATE_KEY}`
     const response = await fetch(`https://flow.ltiaas.com/api/lineitems/${encodeURIComponent(lineItemId)}/scores`, {
         method: 'post',
         body: JSON.stringify({
