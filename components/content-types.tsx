b3e8e893c87462c18e80dd247582f21695745d28
Varun Arora <varunarora.mct@gmail.com>
2023-04-23 12:50:10 -0700

    Remove debug code

--- components/content-types.tsx
+++ components/content-types.tsx
@@ -590,7 +590,6 @@ const Dropdown = ({ setResponse, response, formatting, stepID, body }) => {
       name="dropdown"
       className="block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
       onChange={(event) => {
-          console.log(event.target.value)
           setResponse && setResponse(event.target.value)
       } }
     >
